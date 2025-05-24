a93a706c6456da1bab67c11ac01d87fd5426fca2
Slorrr <86879002+Slorrr@users.noreply.github.com>
2025-05-24 19:25:29 +0300

    rename main class

--- src/main/java/com/bookshop/BookShopApplication.java
+++ src/main/java/com/bookshop/BookShopApplication.java
@@ -4,10 +4,10 @@ import  org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FireServiceApplication {
+public class BookShopApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(FireServiceApplication.class, args);
+        SpringApplication.run(BookShopApplication.class, args);
     }
 
 }
